e432328a070db09f4a83f69c112151c6693c4d45
Andrey Burakov <burakov173@ya.ru>
2026-01-19 22:34:06 +0400

    1

---
+++ src/Show.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Класс Show (спектакль)
+class Show {
+    String title;
+    private int duration;
+    private Director director;
+    List<Actor> listOfActors;
+
+
+    public Show(String title, int duration, Director director) {
+        this.title = title;
+        this.duration = duration; // длительность в минутах
+        this.director = director;
+        this.listOfActors = new ArrayList<>();
+    }
+
+    public void addActor(Actor actor) {
+        if (listOfActors.contains(actor)) {
+            System.out.println("Актёр " + actor + " уже участвует в спектакле «" + title + "».");
+        } else {
+            listOfActors.add(actor);
+            System.out.println("Актёр " + actor + " добавлен в спектакль «" + title + "».");
+        }
+    }
+
+    public void replaceActor(Actor newActor, String surnameToReplace) {
+        int index = -1;
+        for (int i = 0; i < listOfActors.size(); i++) {
+            if (listOfActors.get(i).getSurname().equals(surnameToReplace)) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index == -1) {
+            System.out.println("Актёра с фамилией " + surnameToReplace + " не найдено в спектакле «" + title + "».");
+        } else {
+            listOfActors.set(index, newActor);
+            System.out.println("Актёр с фамилией " + surnameToReplace + " заменён на " + newActor + " в спектакле «" + title + "».");
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Спектакль '" + title + "':\n");
+        sb.append("  Длительность: " + duration + " мин\n");
+        sb.append("  Режиссёр: " + director + "\n");
+        sb.append("  Список актёров:\n");
+
+        for (Actor actor : listOfActors) {
+            sb.append("    - " + actor + "\n");  // используем toString() у Actor
+        }
+
+        return sb.toString();
+    }
+
+    public Director getDirector() {
+        return director;
+    }
+}
---
+++ src/Theatre.java
@@ -0,0 +1,72 @@
+
+public class Theatre {
+    public static void main(String[] args) {
+
+        // 1. Создаём трёх актёров
+        Actor actor1 = new Actor("Иван", "Иванов", Gender.MALE, 1.80);
+        Actor actor2 = new Actor("Мария", "Петрова", Gender.FEMALE, 1.65);
+        Actor actor3 = new Actor("Алексей", "Сидоров", Gender.MALE, 1.75);
+
+        // 2. Создаём двух режиссёров
+        Director director1 = new Director("Андрей", "Смирнов", Gender.MALE, 15);
+        Director director2 = new Director("Елена", "Васильева", Gender.FEMALE, 10);
+
+        // 3. Создаём автора музыки и хореографа
+        String musicAuthor = "Пётр Чайковский";
+        String choreographer = "Юрий Григорович";
+
+
+        // Вывод созданных объектов
+        System.out.println("Актёры:");
+        System.out.println(actor1);
+        System.out.println(actor2);
+        System.out.println(actor3);
+
+
+        System.out.println("\nРежиссёры:");
+        System.out.println(director1);
+        System.out.println(director2);
+
+
+        System.out.println("\nАвтор музыки: " + musicAuthor);
+        System.out.println("Хореограф: " + choreographer);
+
+        // Создаём спектакли
+        Show regularShow = new Show("Гамлет", 180, director1);
+        Opera operaShow = new Opera("Евгений Онегин", 240, director2, musicAuthor, "Текст либретто оперы...", 60);
+        Ballet balletShow = new Ballet("Лебединое озеро", 150, director1, musicAuthor, "Текст либретто балета...", choreographer);
+
+        // Распределяем актёров по спектаклям
+        System.out.println();
+        regularShow.addActor(actor1);
+        regularShow.addActor(actor2);
+
+        operaShow.addActor(actor1);
+        operaShow.addActor(actor3);
+
+        balletShow.addActor(actor2);
+        balletShow.addActor(actor3);
+
+        // Выводим списки актёров для каждого спектакля
+        System.out.println();
+        System.out.println(regularShow);
+
+        // Заменяем актёра в одном из спектаклей
+        System.out.println("Замена актера:");
+        Actor newActor = new Actor("Ольга", "Морозова", Gender.FEMALE, 1.70);
+        operaShow.replaceActor(newActor, "Сидоров");
+
+
+        // Ещё раз выводим список актёров для изменённого спектакля
+        System.out.println(operaShow);
+
+        // Пытаемся заменить несуществующего актёра
+        System.out.println("Ещё замена актера:");
+        regularShow.replaceActor(actor3, "Кузнецов");
+
+        // Выводим либретто для оперного и балетного спектакля
+        System.out.println();
+        operaShow.printLibretto();
+        balletShow.printLibretto();
+    }
+}
